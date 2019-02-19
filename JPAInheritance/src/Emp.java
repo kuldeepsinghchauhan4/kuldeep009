6e6d4cdcd6967f43f9eab30bf9f93c351814ef45
kuldeepsinghchauhan4 <47030280+kuldeepsinghchauhan4@users.noreply.github.com>
2019-02-19 17:33:51 +0530

    Add files via upload

---
+++ JPAInheritance/src/Emp.java
@@ -0,0 +1,61 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="emp_store")
+@Inheritance(strategy=InheritanceType.JOINED)
+
+
+public class Emp {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+private int empId;
+	
+private String empName;
+private float empSal;
+public int getEmpId;
+public int getEmpId() {
+	return empId;
+}
+public void setEmpId(int empId) {
+	this.empId = empId;
+}
+public String getEmpName() {
+	return empName;
+}
+public void setEmpName(String empName) {
+	this.empName = empName;
+}
+public float getEmpSal() {
+	return empSal;
+}
+public void setEmpSal(float empSal) {
+	this.empSal = empSal;
+}
+public int getGetEmpId() {
+	return getEmpId;
+}
+public void setGetEmpId(int getEmpId) {
+	this.getEmpId = getEmpId;
+}
+public Emp(int empId, String empName, float empSal) {
+	super();
+	this.empId = empId;
+	this.empName = empName;
+	this.empSal = empSal;
+	this.getEmpId = getEmpId;
+}
+@Override
+public String toString() {
+	return "Emp [empId=" + empId + ", empName=" + empName + ", empSal=" + empSal + ", getEmpId=" + getEmpId + "]";
+}
+public Emp() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+}
